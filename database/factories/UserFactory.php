6e4a9824d2bb7f80e085a7b34e01eff11825b19a
dvmwebdev <dvmwebdev@gmail.com>
2023-02-17 16:53:53 +0200

    створення фейкових користувачів

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,39 +2,22 @@
 
 namespace Database\Factories;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
-    {
-        return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
-    }
+  public function definition(): array
+  {
+    return [
+      'first_name' => fake()->firstName(),
+      'last_name' => fake()->lastName(),
+      'email' => fake()->email(),
+      'password' => Hash::make('user123'),
+      'created_at' => Carbon::now()->subMonths(random_int(1, 3)),
+      'updated_at' => Carbon::now()->addMonths(random_int(1, 3))
+    ];
+  }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
-    {
-        return $this->state(fn(array $attributes) => [
-            'email_verified_at' => null,
-        ]);
-    }
 }
