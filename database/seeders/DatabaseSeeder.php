8d5f9b15e55959191cd9703341cd6a5126e91c13
dvmwebdev <dvmwebdev@gmail.com>
2023-02-21 19:28:27 +0200

    додано пост сідер

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
   {
     $this->call([
       UserSeeder::class,
-      CategorySeeder::class
+      CategorySeeder::class,
+      PostSeeder::class
     ]);
   }
 }
