1b44a7355b8a54e44928af1c94b04d2a2002257e
dvmwebdev <dvmwebdev@gmail.com>
2023-02-21 15:52:49 +0200

    додано роут всіх категорій

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\Api\v1\AuthController;
+use App\Http\Controllers\Api\v1\CategoryController;
 use App\Http\Controllers\Api\v1\UserController;
 use App\Http\Controllers\InfoController;
 
@@ -18,4 +19,8 @@ Route::group(['middleware' => 'api', 'prefix' => 'auth'], function () {
 Route::group(['prefix' => 'user'], function () {
   Route::get('all-user', [UserController::class, 'index']);
   Route::post('show/{id}', [UserController::class, 'show']);
+});
+
+Route::group(['prefix' => 'category'], function () {
+  Route::get('', [CategoryController::class, 'index']);
 });
